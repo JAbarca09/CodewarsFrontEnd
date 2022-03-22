07bce5b7b75477d5d357d8e5f776579bd7211910
NguyenA001 <annguyen8545@gmail.com>
2022-03-22 13:30:16 -0700

    log in page lay out

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import CodewarsLogo from "../images/CodewarsLogo.svg";
+import "./LogInStyle.css";
+
+export default function LoginPage() {
+  return (
+    <Container fluid className="backgroundColor d-flex align-items-center justify-content-center">
+
+
+
+        <Row>
+          <Col>
+            
+          </Col>
+        </Row>
+      <Row>
+        <Col>
+          <Form>
+            <span className="d-flex justify-content-center mb-5">
+
+            <Form.Group><img src={CodewarsLogo} className="logo" /></Form.Group>
+            </span>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="whiteFont">Username</Form.Label>
+              <Form.Control
+                className="inputWidth"
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className="whiteFont">Password</Form.Label>
+              <Form.Control
+                className="inputWidth"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <span className="d-flex justify-content-center mt-4">
+            <Button variant="primary" type="submit" className="ButtonStyle">
+              Log In
+            </Button>
+            </span>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
