b6d37ea62e2aa4eb7c53778f8d048269ade1f50f
Scotterpop73 <sctzone@aol.com>
2022-03-23 09:32:31 -0700

    Added UserContext Provider

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginPage from "./Pages/LoginPage";
 import Dashboard from "./Pages/Dashboard";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UserContext from "./Context/UserContext";
+import useUser from "./Hooks/use-user";
 
 function App() {
   return (
-    <>
+    <UserContext.Provider value={useUser()}>
       <BrowserRouter>
         {/* <LoginPage/> */}
         <Dashboard/>
@@ -19,7 +21,7 @@ function App() {
           {/* <Route path="dashboard" element={<Dashboard/>} key="dashboard"/> */}
         </Routes>
       </BrowserRouter>
-    </>
+    </UserContext.Provider>
   );
 }
 
