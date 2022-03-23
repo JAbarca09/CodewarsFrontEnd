376b2c82e0696a911a3436c5c6c59839a69af6dc
Scotterpop73 <sctzone@aol.com>
2022-03-23 09:21:33 -0700

    Set up UserContext

---
+++ src/Hooks/use-user.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function useUser(){
+
+    const [userId, setUserId] = useState("");
+    const [cohortName, setCohortName] = useState("");
+    const [codeWarName, setCodeWarName] = useState("");
+    const [isAdmin, setIsAdmin] = useState("");
+    const [isDeleted, setIsDeleted] = useState("");
+    const [userItems, setUserItems] = useState("");
+
+    return { userId, setUserId, cohortName, setCohortName, codeWarName, setCodeWarName, isAdmin, setIsAdmin, isDeleted, setIsDeleted, userItems, setUserItems } 
+}
