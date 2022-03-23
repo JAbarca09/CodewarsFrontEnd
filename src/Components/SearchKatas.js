cc44d4edf45baa4626e88d47b47babc44c3a3725
Jesse <JesseAbarca888@gmail.com>
2022-03-22 21:56:27 -0700

    Fixed the button and centered the kata results

--- src/Components/SearchKatas.js
+++ src/Components/SearchKatas.js
@@ -9,14 +9,14 @@ export default function SearchKatas() {
           <tbody className="whiteFont">
             <tr>
                 <Row>
-                <Col md={4} className="d-flex justify-content-center">
+                <Col md={4} className="mt-2 d-flex justify-content-center">
               <td colSpan={2}>Name of Kata Searched</td>
                 </Col>
-                <Col md={4} className="d-flex justify-content-center">
+                <Col md={4} className="mt-2 d-flex justify-content-center">
                 <td colSpan={2}>Kyu Level of Kata Searched</td>
                 </Col>
                 <Col md={4} className="d-flex justify-content-center">
-                <td colSpan={2}> <Button variant="success" className="reserveButton">Reserve</Button></td>
+                <td colSpan={2}> <Button variant="success" className="">Reserve</Button></td>
                 </Col>
                 </Row>
             </tr>
